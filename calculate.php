0e3afd9191053b0d6da9c0423e64fc4eb2f28bb4
Al Hutchings <4964276@student.swin.edu.au>
2016-10-13 17:11:00 +1100

    added insertion in sql database

--- calculate.php
+++ calculate.php
@@ -1,32 +1,38 @@
 <?php
-//get values from form
+//get name string
 $name = $_GET['a'];
+//get birthday string
 $birthday = $_GET['b'];
+//convert string to date
 $birthday= date('d-m-Y',strtotime($birthday));
 //create database connection
 $con = mysqli_connect('db4free.net','s4964276','28853671','sdeassign1');
 //check datbase connection
 if (!$con) {
+    //return error message if cant connect to the database
     die('Could not connect: ' . mysqli_error($con));
-    echo false;
 }
 else
 {
-  //calculate age and return via xHR object
+  //create todays date as string
     $today = date("d-m-Y");
+  ////calculate age and return via xHR object
     $diff = date_diff(date_create($today),date_create($birthday));
+  //convert times back to strings for sql insertion
+    $birthday=date_create($birthday)->format('Y-m-d');
+    $diff=$diff->format('%a');
   //create query
-  $sql = "INSERT INTO data () values ()";
-  //try query
-  if (mysqli_query($con,$sql)=false)
-  {
-    echo false;
-  }
-  else
-  {
+    $sql = "INSERT INTO data (name, age, birthday) VALUES ('$name' ,'$diff' , '$birthday')";
+    if (mysqli_query($con,$sql)===false)
+    {
+    //return error
+      die('Could not insert data: ' . mysqli_error($con));
+    }
+    else
+    {
     //return age if inserted into data correctly
-    echo $diff->format('%a');
-  }
+    echo $diff;
+    }
   //close connection
   mysqli_close($con);
 }
